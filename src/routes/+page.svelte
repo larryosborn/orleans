9ba975757b748bfad3ffb15abaac41ead00b17d4
Larry Osborn <larry@cornsyrup.org>
2023-03-03 00:17:03 -0500

    feat: home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Orleans</h1>
+			<p class="py-6">I am a town.</p>
+		</div>
+	</div>
+</div>
