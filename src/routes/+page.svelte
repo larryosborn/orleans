9d2321fcd280c0cdb45ebdef07d29ec3d3e4421b
Larry Osborn <385770+larryosborn@users.noreply.github.com>
2024-11-23 09:39:37 -0500

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
-<div class="hero min-h-screen bg-base-200">
-	<div class="hero-content text-center">
-		<div class="max-w-md">
-			<h1 class="text-5xl font-bold">Orleans</h1>
-			<p class="py-6">I am a town.</p>
-		</div>
+<div class="flex h-full items-center justify-center">
+	<div class="flex flex-col items-center justify-center gap-3">
+		<h1 class="text-5xl font-bold">Orleans</h1>
+		<p>I am a town.</p>
 	</div>
 </div>
